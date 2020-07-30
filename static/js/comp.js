a92cd100ee3a2c6f26e758ca7c373fe13833ec55
athompson00 <athompson00@stuy.edu>
2020-07-29 21:49:50 -0400

    added update weights

--- static/js/comp.js
+++ static/js/comp.js
@@ -1,4 +1,28 @@
 function updateWeighting(){
+  console.log(converted);
   var listed = document.getElementsByClassName("form-control w-100");
-  console.log(listed);
+  // console.log(listed);
+  var keylist = Object.keys(converted['holdings']);
+  var percentIn = 0;
+  for (item in listed){
+    console.log(keylist[item]);
+    if (! isNaN(item)){
+      converted['holdings'][keylist[item]]['Weight'] = listed[item].value;
+      percentIn += listed[item].value;
+    }
+  }
+  console.log(converted);
+  const graph2 = document.getElementById("graph2");
+  while (graph2.firstChild) {
+    graph2.removeChild(graph2.lastChild);
+  }
+  converted['excludedWeight'] = 100 - percentIn;
+  converted['includedWeight'] = percentIn;
+  compileETF(converted);
+  document.getElementById("title1").innerHTML = "Displaying graph of " + converted["Ticker"] + " made up of its parts weighted by you";
+  if (converted['excludedWeight'] != 0){
+    document.getElementById("label1").innerHTML = "This balance depicts an ETF with your weighting and excludes " + converted['excludedWeight'].toFixed(3) + "% of it's price because the weightings don't add up to one";
+  } else {
+    document.getElementById("label1").innerHTML = "This balance depicts an ETF with your weighting as shown below";
+  }
 }
